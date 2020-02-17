9006418c69d95e125861bfdaafc2d601ecf86c4f
Jmiquilena <gregoriomiqui@gmail.com>
2020-02-17 13:40:47 -0400

    adjusted profiling

--- excel_test.js
+++ excel_test.js
@@ -1,53 +1,74 @@
-const fs = require('fs');
-const Excel = require('exceljs');
+const os = require('os-utils')
 
-var options = {
-    filename: './docs/report.xlsx',
-    useStyles: true,
-    useSharedStrings: true
-};
+var initialMemoryUsage = 100 - os.freememPercentage() * 100
+var initialCPU = 0
 
-var workbook = new Excel.stream.xlsx.WorkbookWriter(options);
-let sheet = workbook.addWorksheet('First WS');
+main()
 
-let path_ = './data/'
-let filename = 'data'
-let files = ['0', '1', '2', '3', '4']
-let counter = 0
-
-console.time('it took')
-files.forEach(file => {
-    fs.readFile(`${path_}${filename}_${file}.json`, (err, data) => {
-        if (err) throw err;
-
-        let users = JSON.parse(data);
-        if (!users) {
-            console.error('JSON Parse Error or empty source File');
-            process.exit();
+function main() {
+    var profiling = setInterval(() => {
+        if (!flag) {
+            console.log('\tCPU\tMEM');
+            flag = true
         }
+        os.cpuUsage(function (v) {
+            console.log(`\t${((v - initialCPU) * 100).toFixed(2) + ''}\t${(((1 - os.freememPercentage()) * 100) - initialMemoryUsage).toFixed(2)}`)
+        })
+    }, 100)
 
-        if (sheet.columns == null) {
-            let keys = Object.keys(users[0]), columns = []
-            for (let i = 0; i < keys.length; i++) {
-                columns.push({
-                    'header': keys[i].toUpperCase(),
-                    'key': keys[i],
-                });
+    console.time('total')
+    console.time('startup')
+    const fs = require('fs');
+    const Excel = require('exceljs');
+
+    var options = {
+        filename: './docs/report.xlsx',
+        useStyles: true,
+        useSharedStrings: true
+    };
+
+    var workbook = new Excel.stream.xlsx.WorkbookWriter(options);
+    let sheet = workbook.addWorksheet('First WS');
+
+    let path_ = './data/'
+    let filename = 'data'
+    let files = ['0', '1', '2', '3', '4']
+    let counter = 0
+    let flag = false
+
+    console.timeEnd('startup')
+    files.forEach(file => {
+        fs.readFile(`${path_}${filename}_${file}.json`, (err, data) => {
+            if (err) throw err;
+
+            let users = JSON.parse(data);
+            if (!users) {
+                return console.error('JSON Parse Error or empty source File');
             }
-            sheet.columns = columns;
-        }
 
-        users.forEach(user => {
-            sheet.addRow(user).commit()
-        });
+            if (sheet.columns == null) {
+                let keys = Object.keys(users[0]), columns = []
+                for (let i = 0; i < keys.length; i++) {
+                    columns.push({
+                        'header': keys[i].toUpperCase(),
+                        'key': keys[i],
+                    });
+                }
+                sheet.columns = columns;
+            }
 
-        //sheet.commit()
-        console.log(`sheet ${file} done`)
-        if (++counter == files.length) {
-            workbook.commit()
-                .then(() => {
-                    console.timeEnd('it took')
-                })
-        }
+            users.forEach(user => {
+                sheet.addRow(user).commit()
+            });
+
+            //sheet.commit()
+            if (++counter == files.length) {
+                workbook.commit()
+                    .then(() => {
+                        clearInterval(profiling)
+                        console.timeEnd('total')
+                    })
+            }
+        })
     })
-})
+}
--- index.js
+++ index.js
@@ -1,82 +1,111 @@
-const createPhantomPool = require('phantom-pool')
-const async = require('asyncawait/async');
-const await = require('asyncawait/await');
-const fs = require('fs');
-const ejs = require('ejs');
-const merge = require('easy-pdf-merge');
+const os = require('os-utils')
 
-let exp = /.+\.pdf$/
-let docDir = './docs'
-
-const pool = createPhantomPool({
-    max: 10,
-    min: 2,
-    idleTimeoutMillis: 30000,
-    maxUses: 50,
-    validator: (resource) => Promise.resolve(true),
-    testOnBorrow: true,
-    phantomArgs: [['--ignore-ssl-errors=true', '--disk-cache=true'],
-        //{logLevel: 'debug',}
-    ],
+var initialMemoryUsage = 100 - os.freememPercentage() * 100
+os.cpuUsage((v) => {
+    initialCPU = v;
+    main();
 })
-let dataDir = './data/'
-let filename = 'data'
-let files = ['0', '1', '2', '3', '4']
-let counter = 0
 
-function merge_pdfs(pathArray, output) {
-    merge(pathArray, output, (err) => {
-        if (err)
-            return console.log(err)
+function main() {
+    const createPhantomPool = require('phantom-pool')
+    const async = require('asyncawait/async');
+    const await = require('asyncawait/await');
+    const fs = require('fs');
+    const ejs = require('ejs');
+    const merge = require('easy-pdf-merge');
 
-        console.timeEnd('merge took')
-        console.timeEnd('total')
+    let exp = /temp_.+\.pdf$/
+    let docDir = './docs'
+
+    const pool = createPhantomPool({
+        max: 10,
+        min: 2,
+        idleTimeoutMillis: 30000,
+        maxUses: 50,
+        validator: (resource) => Promise.resolve(true),
+        testOnBorrow: true,
+        phantomArgs: [['--ignore-ssl-errors=true', '--disk-cache=true'],
+            //{logLevel: 'debug',}
+        ],
+    })
+    let dataDir = './data/'
+    let filename = 'data'
+    let files = ['0', '1', '2', '3', '4']
+    let counter = 0
+    flag = false
+
+    var profiling = setInterval(() => {
+        if (!flag) {
+            console.log('\tCPU\tMEM');
+            flag = true
+        }
+        os.cpuUsage(function (v) {
+            console.log(`\t${((v - initialCPU) * 100).toFixed(2) + ''}\t${(((1 - os.freememPercentage()) * 100) - initialMemoryUsage).toFixed(2)}`)
+        })
+    }, 500)
+
+    function cleanup(){
+        files.forEach(file => {
+            fs.unlink(`./docs/temp_${file}.pdf`, () => {
+                //
+            })
+        })
+    }
+
+    function merge_pdfs(pathArray, output) {
+        merge(pathArray, output, (err) => {
+            if (err)
+                return console.log(err)
+            clearInterval(profiling)
+            console.timeEnd('merge took')
+            console.timeEnd('total')
+            cleanup()
+        })
+    }
+    console.time('total')
+    console.time('initial generation took')
+    files.forEach(file => {
+        fs.readFile(`${dataDir}${filename}_${file}.json`, 'utf-8', (err, data) => {
+            if (err)
+                throw err;
+
+            rows = JSON.parse(data)
+            columns = Object.keys(rows[0])
+            ejs.renderFile('./html/template.ejs', {
+                rows: rows,
+                columns: columns
+            })
+                .then(html => {
+                    pool.use(async((phantom) => {
+                        const page = await(phantom.createPage())
+                        page.property('paperSize', {
+                            format: 'Letter',
+                            header: {
+                                height: "2cm",
+                                contents: phantom.callback(function (pageNum, numPages) {
+                                    return "<h6>Header <span style='float:right'>" + pageNum + " / " + numPages + "</span></h1>";
+                                })
+                            },
+                            footer: {
+                                height: "2cm",
+                                contents: phantom.callback(function (pageNum, numPages) {
+                                    return "<h6>Footer <span style='float:right'>" + pageNum + " / " + numPages + "</span></h1>";
+                                })
+                            }
+                        })
+                        await(page.property('content', html))
+                        await(page.render(`./docs/temp_${file}.pdf`))
+                    }))
+                        .then(() => {
+                            if (++counter == files.length) {
+                                pool.drain().then(() => pool.clear())
+                                console.timeEnd('initial generation took')
+                                console.time('merge took')
+                                merge_pdfs(fs.readdirSync(docDir).filter(dirent => exp.test(dirent)).map(dirent => docDir + '/' + dirent), docDir + '/' + 'report_v1.pdf')
+                            }
+                        })
+
+                })
+        })
     })
 }
-console.time('total')
-console.time('initial generation took')
-files.forEach(file => {
-    fs.readFile(`${dataDir}${filename}_${file}.json`, 'utf-8', (err, data) => {
-        if (err)
-            throw err;
-
-        rows = JSON.parse(data)
-        columns = Object.keys(rows[0])
-        ejs.renderFile('./html/template.ejs', {
-            rows: rows,
-            columns: columns
-        })
-            .then(html => {
-                pool.use(async((phantom) => {
-                    const page = await(phantom.createPage())
-                    page.property('paperSize', {
-                        format: 'Letter',
-                        header: {
-                            height: "2cm",
-                            contents: phantom.callback(function (pageNum, numPages) {
-                                return "<h6>Header <span style='float:right'>" + pageNum + " / " + numPages + "</span></h1>";
-                            })
-                        },
-                        footer: {
-                            height: "2cm",
-                            contents: phantom.callback(function (pageNum, numPages) {
-                                return "<h6>Footer <span style='float:right'>" + pageNum + " / " + numPages + "</span></h1>";
-                            })
-                        }
-                    })
-                    await(page.property('content', html))
-                    await(page.render(`./docs/report_${file}.pdf`))
-                }))
-                    .then(() => {
-                        console.log('#' + file + ' done')
-                        if (++counter == files.length) {
-                            pool.drain().then(() => pool.clear())
-                            console.timeEnd('initial generation took')
-                            console.time('merge took')
-                            merge_pdfs(fs.readdirSync(docDir).filter(dirent => exp.test(dirent)).map(dirent => docDir + '/' + dirent), docDir + '/' + 'report.pdf')
-                        }
-                    })
-
-            })
-    })
-})
