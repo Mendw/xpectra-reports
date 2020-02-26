a072c41624eda6986861ed6a050a57c620a89f27
Jmiquilena <gregoriomiqui@gmail.com>
2020-02-26 11:00:59 -0400

    refactorizacion del prototipo de excel

--- xslx_generation.js
+++ xslx_generation.js
@@ -1,74 +1,54 @@
-const os = require('os-utils')
+const fs = require('fs');
+const Excel = require('exceljs');
 
-var initialMemoryUsage = 100 - os.freememPercentage() * 100
-var initialCPU = 0
+var options = {
+    filename: './docs/report.xlsx',
+    useStyles: true,
+    useSharedStrings: true
+};
 
-main()
+var workbook = new Excel.stream.xlsx.WorkbookWriter(options);
+let sheet = workbook.addWorksheet('First WS');
 
-function main() {
-    var profiling = setInterval(() => {
-        if (!flag) {
-            console.log('\tCPU\tMEM');
-            flag = true
+let data_file_regex = /\.json$/
+let data_directory = './data'
+
+let data_files = fs.readdirSync(data_directory, 'utf-8')
+    .filter(filename => data_file_regex.test(filename))
+    .map(   filename => `${data_directory}/${filename}`)
+
+let counter = 0
+
+data_files.forEach(file => {
+    fs.readFile(file, (err, data) => {
+        if (err) throw err;
+
+        let users = JSON.parse(data);
+        if (!users) {
+            return console.error('JSON Parse Error or empty source File');
         }
-        os.cpuUsage(function (v) {
-            console.log(`\t${((v - initialCPU) * 100).toFixed(2) + ''}\t${(((1 - os.freememPercentage()) * 100) - initialMemoryUsage).toFixed(2)}`)
-        })
-    }, 100)
 
-    console.time('total')
-    console.time('startup')
-    const fs = require('fs');
-    const Excel = require('exceljs');
-
-    var options = {
-        filename: './docs/report.xlsx',
-        useStyles: true,
-        useSharedStrings: true
-    };
-
-    var workbook = new Excel.stream.xlsx.WorkbookWriter(options);
-    let sheet = workbook.addWorksheet('First WS');
-
-    let path_ = './data/'
-    let filename = 'data'
-    let files = fs.readdirSync('./data', 'utf-8')
-    let counter = 0
-    let flag = false
-
-    console.timeEnd('startup')
-    files.forEach(file => {
-        fs.readFile(`${path_}${file}`, (err, data) => {
-            if (err) throw err;
-
-            let users = JSON.parse(data);
-            if (!users) {
-                return console.error('JSON Parse Error or empty source File');
+        if (sheet.columns == null) {
+            let keys = Object.keys(users[0]), columns = []
+            for (let i = 0; i < keys.length; i++) {
+                columns.push({
+                    'header': keys[i].toUpperCase(),
+                    'key': keys[i],
+                });
             }
+            sheet.columns = columns;
+        }
 
-            if (sheet.columns == null) {
-                let keys = Object.keys(users[0]), columns = []
-                for (let i = 0; i < keys.length; i++) {
-                    columns.push({
-                        'header': keys[i].toUpperCase(),
-                        'key': keys[i],
-                    });
-                }
-                sheet.columns = columns;
-            }
+        while(users.length > 0){
+            user = users.pop()
+            sheet.addRow(user).commit()
+        }
 
-            users.forEach(user => {
-                sheet.addRow(user).commit()
-            });
-
-            //sheet.commit()
-            if (++counter == files.length) {
-                workbook.commit()
-                    .then(() => {
-                        clearInterval(profiling)
-                        console.timeEnd('total')
-                    })
-            }
-        })
+        if (++counter == data_files.length) {
+            workbook.commit()
+                .then(() => {
+                    //
+                })
+        }
     })
-}
+})
