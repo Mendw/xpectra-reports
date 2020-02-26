ab15fce52c95fbde825f6b4c8dd0e70f720e1d6a
Jmiquilena <gregoriomiqui@gmail.com>
2020-02-26 13:45:28 -0400

    solved legacy errors

--- legacy_pdf_generation.js
+++ legacy_pdf_generation.js
@@ -5,8 +5,8 @@ const fs = require('fs');                                   // permite trabajar 
 const ejs = require('ejs');                                 // permite generar codigo HTML en base a un plantilla y datos
 const merge = require('easy-pdf-merge');                    // permite combinar varios archivos .pdf 
 
-let temp_pdf_regex = /temp_.+\.pdf$/ 
-let data_file_regex = /\.json$/
+let temp_pdf_regex = /temp_.+\.pdf$/
+let data_file_regex = /.+\.json$/
 
 let document_directory = './docs'
 let data_directory = './data'
@@ -14,6 +14,7 @@ let data_directory = './data'
 let template = './html/template.ejs'
 
 let counter = 0
+let filename_suffix = 0
 
 // crea una pool de instancias de phantom para agilizar el proceso de renderizado de archivos HTML
 const pool = createPhantomPool({    // configuración la 'pool' de instancias de phantom
@@ -32,18 +33,18 @@ function merge_pdfs(pathArray, output) { // recibe un arreglo de rutas relativas
     merge(pathArray, output, (err) => {  // usa la función merge de la librería 'easy-pdf-merge' para combinar los archivos
         if (err)
             return console.log(err)
+
+        pathArray.forEach(file => {
+            fs.unlinkSync(file)
+        })
     })
 
-    pathArray.forEach(file => {         // elimina los archivos temporales
-        fs.unlink(file)
-    })
 }
 
 // crea una lista de rutas relativas a archivos de datos
 let data_files = fs.readdirSync(data_directory, 'utf-8')        // lee el directorio de datos, creando una lista de nombres de archivos y carpetas
     .filter(filename => data_file_regex.test(filename))         // elimina todos los nombres que no cumplan con la expresión regular
-    .map(  filename => `${data_directory}/${filename}`);        // crea rutas relativas en base al nombre de cada archivo
-
+    .map(filename => `${data_directory}/${filename}`);        // crea rutas relativas en base al nombre de cada archivo
 
 data_files.forEach(filename => {                                // lee cada archivo del arreglo creado arriba
     fs.readFile(filename, 'utf-8', (err, data) => {
@@ -75,13 +76,13 @@ data_files.forEach(filename => {                                // lee cada arch
                         }
                     })                                                                          // configura la página
                     await(page.property('content', html))                                       // añade el contenido
-                    await(page.render(`${document_directory}/temp_${counter}.pdf`))             // renderiza
+                    await(page.render(`${document_directory}/temp_${filename_suffix++}.pdf`))             // renderiza
                 }))
-                    .then(() => {                                                               
+                    .then(() => {
                         if (++counter == data_files.length) {                                   // una vez que ha terminado de renderizar todo
                             pool.drain().then(() => pool.clear())                               // destruye la pool
                             merge_pdfs(                                                         // combina los archivos .pdf creados
-                                fs.readdirSync(document_directory)                              // cuyos nombres son extraidos de la lectura del directorio donde se guardan
+                                fs.readdirSync(document_directory, 'utf-8')                     // cuyos nombres son extraidos de la lectura del directorio donde se guardan
                                     .filter(filename => temp_pdf_regex.test(filename))          // se usa una expresion regular para obtener solo los archivos .pdf temporales
                                     .map(filename => document_directory + '/' + filename),
                                 document_directory + '/' + 'legacy_report.pdf')                 // el nombre del archivo de salida. se guarda en el directorio de documentos.
